760fb9429e95a6b3912e9c4b33762b539f755596
mohmed <mohmedamged9@gmail.com>
2022-10-24 21:35:28 +0200

    build the static UI for the components (store,categories,products,product)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,21 @@ import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { FooterComponent } from './footer/footer.component';
 import { LogInComponent } from './log-in/log-in.component';
+import { StoreComponent } from './store/store.component';
+import { CategoriesComponent } from './store/categories/categories.component';
+import { ProductsComponent } from './store/products/products.component';
+import { ProductComponent } from './store/product/product.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     FooterComponent,
-    LogInComponent
+    LogInComponent,
+    StoreComponent,
+    CategoriesComponent,
+    ProductsComponent,
+    ProductComponent
   ],
   imports: [
     BrowserModule,
